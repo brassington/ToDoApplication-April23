3d820c1063a14e5783804a79bbba260949e620da
J. Andrew Brassington <jabbrass@zoho.com>
2014-04-23 11:22:11 -0500

    Init Commit

---
+++ ToDoApp-April22/main.js
@@ -0,0 +1,20 @@
+﻿//destination += value
+//var filling = "Andrew";
+//'<p>' + filling + '</p>'
+"use strict";
+var toDoCounter = 0;
+
+var elementDoSomething = function () {
+    "use strict";
+    toDoCounter = toDoCounter + 1;
+    var inputValue = document.getElementById("todoInput");
+    var outputOrderedList = document.getElementById("todoUnorderedList");
+
+    outputOrderedList.innerHTML = outputOrderedList.innerHTML +
+        '<li id="item' + toDoCounter + '" onclick="strikeOutListItem(\'item' + toDoCounter + '\');">' + inputValue.value + '</li>';
+};
+
+var strikeOutListItem = function (listItemId) {
+    "use strict";
+    document.getElementById(listItemId).setAttribute("class", "strikethrough");
+};
